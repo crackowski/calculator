06c44802122d6c8dcc39a71c23000cf278b45526
Rubin <rubinimeri6@gmail.com>
2023-06-23 21:07:09 +0200

    Add operate function
    The operate function decides which of
    the above functions to call by taking
    an extra variable 'operator'

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+
+// Created three variables that hold the 3 parts of
+// a math equation
+
+let firstNum;
+let operator;
+let secondNum;
+
+// Function that accepts an operator on top of
+// the first number and second number so it will
+// know which of the above functions to call
+
+function operate(firstNum, operator, secondNum){
+    if(operator === "+") 
+        return add(firstNum, secondNum);
+    else if(operator === "-") 
+        return subtract(firstNum, secondNum);
+    else if(operator === "*")
+        return multiply(firstNum, secondNum);
+    else
+        return divide(firstNum, secondNum);
+}
+
+console.log(operate(5, "*", 5));
