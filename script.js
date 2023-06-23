5050b753514a12442a12e008ef34998b66da1b29
Rubin <rubinimeri6@gmail.com>
2023-06-23 20:58:35 +0200

    Create 4 functions that do basic math
    They all accept two parameters

---
+++ script.js
@@ -0,0 +1,23 @@
+//Function that adds two numbers
+
+function add(a, b){
+    return a + b;
+}
+
+//function that subtracts two numbers
+
+function subtract(a, b){
+    return a - b;
+}
+
+//Function that multiplies two numbers
+
+function multiply(a, b){
+    return a * b;
+}
+
+//Function that divides two numbers
+
+function divide(a, b){
+    return a / b;
+}
