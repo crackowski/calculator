1d420670cdd6eba2e51e6296fd6dca663f07e0cf
Rubin <rubinimeri6@gmail.com>
2023-06-25 14:35:15 +0200

    Add event listener for buttons
    Works if you do every single operation
    by pressing the '=' sign, won't work
    if you string together multiple operators

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ function divide(a, b){
 // Created three variables that hold the 3 parts of
 // a math equation
 
-let firstNum;
+let firstNum = 0;
 let operator;
-let secondNum;
+let secondNum = 0;
 
 // Function that accepts an operator on top of
 // the first number and second number so it will
@@ -46,3 +46,50 @@ function operate(firstNum, operator, secondNum){
 }
 
 console.log(operate(5, "*", 5));
+
+let digits = Array.from(document.querySelectorAll(".container .buttons .digit"));
+let displayValue = document.querySelector(".display");
+
+let operators = Array.from(document.querySelectorAll(".container .buttons .operator"));
+let equals = document.querySelector(".equals");
+
+// Add event listener to digits
+
+digits.forEach(digit => {
+    digit.addEventListener("click", () => {
+        if(operator !== undefined){
+            secondNum *= 10;
+            secondNum += +digit.textContent
+            displayValue.textContent = secondNum;
+            console.log(secondNum);
+        }
+        else{
+        firstNum *= 10;
+        firstNum += +digit.textContent;
+        displayValue.textContent = firstNum;
+        console.log(firstNum);
+        }
+    })
+});
+
+// Add event listener to operators;
+
+operators.forEach(op => {
+    op.addEventListener("click", () => {
+            operator = op.textContent;
+            console.log(operator);
+        
+    })
+});
+
+// Add event listener to equals sign
+
+equals.addEventListener("click", () => {
+    if(firstNum !== 0 && secondNum !== 0){
+        displayValue.textContent = operate(firstNum, operator, secondNum);
+        operator = undefined;
+        firstNum = +displayValue.textContent;
+        secondNum = 0;
+    }
+})
+
